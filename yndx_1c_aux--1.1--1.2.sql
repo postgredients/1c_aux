f1b8e41c068b5ff3b79ba054bab29c4d35e916e3
reshke <reshke@double.cloud>
2026-03-04 17:00:08 +0000

    Revert "Fix extension for pre-12 versions"
    This reverts commit cd9aaffbaaf89b462e02f8836a78bbb256fc8356.

--- yndx_1c_aux--1.1--1.2.sql
+++ yndx_1c_aux--1.1--1.2.sql
@@ -30,16 +30,14 @@ CREATE FUNCTION state_min_bytea(st bytea, inp bytea) RETURNS bytea
          end if;
     END;$$;
 
-DROP AGGREGATE IF EXISTS max(bytea);
-CREATE AGGREGATE max (
+CREATE OR REPLACE AGGREGATE max (
 	BASETYPE	= 	bytea,
 	SFUNC 		= 	state_max_bytea,
 	STYPE		= 	bytea,
 	SORTOP		= 	'>'
 );
 
-DROP AGGREGATE IF EXISTS min(bytea);
-CREATE AGGREGATE min (
+CREATE OR REPLACE AGGREGATE min (
 	BASETYPE	= 	bytea,
 	SFUNC 		= 	state_min_bytea,
 	STYPE		= 	bytea,
